f9a70876cdf78139e3a897ceb6c882b088587b3a
varshanbp <varshanbp@gmail.com>
2016-03-16 01:19:45 +0530

    Minor UI and Functionality Change 7

--- includes/auto-script/auto-script2.php
+++ includes/auto-script/auto-script2.php
@@ -4,6 +4,8 @@ require_once('../db-conn.php');
 
 date_default_timezone_set('Asia/Calcutta');
 
-$
+$sql_qry1="CREATE TABLE IF NOT EXISTS `roles` ( `regno` VARCHAR(10) NOT NULL ,
+     `dept` CHAR(2) NOT NULL , `sems` INT(1) NOT NULL , `role` VARCHAR(10) ,
+     `sub1` VARCHAR(7) , `sub2` VARCHAR(7) ,`sub3` VARCHAR(7) )ENGINE = InnoDB;
 
 ?>
